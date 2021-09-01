c969729fc053cb2de6ee18dda6c7b489a764a9a3
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2021-09-01 09:52:58 +0530

    Update 1.selection sort.py

--- SORTING ALGORITHMS/1.selection sort.py
+++ SORTING ALGORITHMS/1.selection sort.py
@@ -10,8 +10,8 @@ def selectionsort(l):
 l=list(range(5000,0,-1))
 print(selectionsort(l))
 
+"""(This works well and good for values below 3000 when we give 5000 numbers in a list
+it takes so a little time for running . if we consider in a real world scenario out of 1 lakh number such that
+it takes so much time to execute . 
 
-""" Here it gets the output a little bit quicker than before.
-Since it checks every element before going into loop
-But in the both cases Time complexity is  O(n^2) . It isn't feasible for n over 5000
-Here comes the other one Merge Sort """
+Here we go into another strategy known as insertion sort """
