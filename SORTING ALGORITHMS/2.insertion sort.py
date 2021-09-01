c364b4904414756841c96d71da59553582df9bc3
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2021-09-01 09:53:29 +0530

    Update 2.insertion sort.py

--- SORTING ALGORITHMS/2.insertion sort.py
+++ SORTING ALGORITHMS/2.insertion sort.py
@@ -8,3 +8,9 @@ def insertionsort(l):
 #print(insertionsort([10,12,4,10,8,13]))
 l=list(range(5000,0,-1))
 print(insertionsort(l))
+
+
+""" Here it gets the output a little bit quicker than before.
+Since it checks every element before going into loop
+But in the both cases Time complexity is  O(n^2) . It isn't feasible for n over 5000
+Here comes the other one Merge Sort """
