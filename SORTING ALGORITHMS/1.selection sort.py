01c248a60659618d6c5b688dbfd0940de2737980
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2021-09-01 09:53:08 +0530

    Update 1.selection sort.py

--- SORTING ALGORITHMS/1.selection sort.py
+++ SORTING ALGORITHMS/1.selection sort.py
@@ -10,7 +10,7 @@ def selectionsort(l):
 l=list(range(5000,0,-1))
 print(selectionsort(l))
 
-"""(This works well and good for values below 3000 when we give 5000 numbers in a list
+"""This works well and good for values below 3000 when we give 5000 numbers in a list
 it takes so a little time for running . if we consider in a real world scenario out of 1 lakh number such that
 it takes so much time to execute . 
 
